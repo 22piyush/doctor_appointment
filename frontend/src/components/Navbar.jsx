46fd50416424b960ee4dd23c47dd70ebad3d3802
22piyush <piyushaglawe05@gmail.com>
2026-01-29 22:58:13 +0530

    done

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,34 +1,34 @@
-import React from 'react'
-import { assets } from '../assets/assets_frontend/assets'
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import { assets } from "../assets/assets_frontend/assets";
+import { NavLink } from "react-router-dom";
 
 function Navbar() {
   return (
-    <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400'>
-        <img className='w-44 cursor-pointer' src={assets.logo} alt="logo_img"/>
-        <ul className='hidden md:flex items-center gap-5 font-medium'>
-            <NavLink>
-                <li>Home</li>
-                <hr/>
-            </NavLink>
-             <NavLink>
-                <li>All Doctors</li>
-                <hr/>
-            </NavLink>
-             <NavLink>
-                <li>About</li>
-                <hr/>
-            </NavLink>
-                <NavLink>
-                <li>Contact</li>
-                <hr/>
-            </NavLink>
-        </ul>
-        <div>
-            <button>Create Account</button>
-        </div>
+    <div className="flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400">
+      <img className="w-44 cursor-pointer" src={assets.logo} alt="logo_img" />
+      <ul className="hidden md:flex items-center gap-5 font-medium">
+        <NavLink>
+          <li className="py-1 ">Home</li>
+          <hr className="border-none outline-none h-0.5 bg-[#5F6FFF] w-3/5 m-auto hidden "/>
+        </NavLink>
+        <NavLink>
+          <li className="py-1 ">All Doctors</li>
+          <hr className="border-none outline-none h-0.5 bg-[#5F6FFF] w-3/5 m-auto hidden "/>
+        </NavLink>
+        <NavLink>
+          <li className="py-1 ">About</li>
+          <hr className="border-none outline-none h-0.5 bg-[#5F6FFF] w-3/5 m-auto hidden "/>
+        </NavLink>
+        <NavLink>
+          <li className="py-1 ">Contact</li>
+          <hr className="border-none outline-none h-0.5 bg-[#5F6FFF] w-3/5 m-auto hidden "/>
+        </NavLink>
+      </ul>
+      <div>
+        <button>Create Account</button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
