4ff7273371e87f0e2fa9752f2a7d486eefc08bd9
22piyush <piyushaglawe05@gmail.com>
2026-01-29 22:29:46 +0530

    done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
 
 function App() {
   return (
-    <div className='bg-amber-800'>App</div>
+    <div className='mx-4 sm:mx-[10%]'>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
+    </div>
   )
 }
 
