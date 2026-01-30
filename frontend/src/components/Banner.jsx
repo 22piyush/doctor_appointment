9eb1e4d56548fcbf96004c152d0a823e1705e2b6
22piyush <piyushaglawe05@gmail.com>
2026-01-30 23:11:53 +0530

    done

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -5,17 +5,17 @@ function Banner() {
   return (
     <div className="flex bg-[#5F6FFF] rounded-lg px-6 sm:px-10 md:px-14 lg:px-12 my-20 md:mx-10">
       {/* Left Side */}
-      <div>
-        <div>
+      <div className="flex-1 py-8 sm:py-10 md:py-16 lg:py-24 lg:pl-5">
+        <div className="text-xl sm:text-2xl md:text-3xl lg:text-5xl font-semibold text-white">
             <h1>Book Appointment</h1>
-            <p>with 100+ Trustd Doctors</p>
+            <p className="mt-4">with 100+ Trustd Doctors</p>
         </div>
-        <button>Create account</button>
+        <button className="bg-white text-sm sm:text-base text-gray-600 px-8 py-3 rounded-full mt-6 hover:scale-105 transition-all">Create account</button>
       </div>
 
       {/* Right Side */}
-      <div>
-        <img src={assets.appointment_img} alt="appointment_img" />
+      <div className="hidden md:block md:w-1/2 lg:w-[370px] relative">
+        <img className="w-full absolute bottom-0 right-0 max-w-md" src={assets.appointment_img} alt="appointment_img" />
       </div>
     </div>
   );
