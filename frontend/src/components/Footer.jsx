43a5ff700dd31b50773129cc11c0c1298de441dc
22piyush <piyushaglawe05@gmail.com>
2026-01-30 23:18:48 +0530

    done

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+        <div>
+            {/* Left Section  */}
+            <div>
+                
+            </div>
+
+            {/* Center Section  */}
+            <div></div>
+
+            {/* Right Section  */}
+            <div></div>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
