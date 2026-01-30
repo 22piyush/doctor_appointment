8211ba02f3aca806611f299bc98dc050e938403f
22piyush <piyushaglawe05@gmail.com>
2026-01-30 23:31:32 +0530

    done

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,12 +3,12 @@ import { assets } from "../assets/assets_frontend/assets";
 
 function Footer() {
   return (
-    <div>
-      <div>
+    <div className="md:mx-10">
+      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
         {/* Left Section  */}
         <div>
-          <img src={assets.logo} alt="logo" />
-          <p>
+          <img className="mb-5 w-40" src={assets.logo} alt="logo" />
+          <p className="w-full md:w-2/3 text-gray-600 leading-6">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
             harum sit unde ab tempora error perspiciatis, recusandae est,
             doloribus dolores delectus. Esse magnam illum, odio hic ad possimus
@@ -20,8 +20,8 @@ function Footer() {
 
         {/* Center Section  */}
         <div>
-          <p>COMPANY</p>
-          <ul>
+          <p className="text-xl font-medium mb-5">COMPANY</p>
+          <ul className="flex flex-col gap-2 text-gray-600">
             <li>Home</li>
             <li>About</li>
             <li>Contact Us</li>
@@ -31,8 +31,8 @@ function Footer() {
 
         {/* Right Section  */}
         <div>
-          <p>GET IN TOUCH</p>
-          <ul>
+          <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
+          <ul className="flex flex-col gap-2 text-gray-600">
             <li>123456789920</li>
             <li>dada#gmaimm.com</li>
           </ul>
@@ -42,7 +42,7 @@ function Footer() {
       {/* Comment  */}
       <div>
         <hr />
-        <p>Copyright 2026@ Prescripto - All Right Reserved.</p>
+        <p className="py-5 text-sm text-center">Copyright 2026@ Prescripto - All Right Reserved.</p>
       </div>
     </div>
   );
