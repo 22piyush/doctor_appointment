97f6803746afc92557547eff65cc33312e84784b
22piyush <piyushaglawe05@gmail.com>
2026-01-30 23:01:37 +0530

    done

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import { assets } from "../assets/assets_frontend/assets";
 
 function Banner() {
   return (
-    <div>Banner</div>
-  )
+    <div className="flex bg-[#5F6FFF] rounded-lg px-6 sm:px-10 md:px-14 lg:px-12 my-20 md:mx-10">
+      {/* Left Side */}
+      <div>
+        <div>
+            <h1>Book Appointment</h1>
+            <p>with 100+ Trustd Doctors</p>
+        </div>
+        <button>Create account</button>
+      </div>
+
+      {/* Right Side */}
+      <div>
+        <img src={assets.appointment_img} alt="appointment_img" />
+      </div>
+    </div>
+  );
 }
 
-export default Banner
+export default Banner;
