03e611302adf8b91e6a030c3b1489bf08cec6555
22piyush <piyushaglawe05@gmail.com>
2026-01-31 20:52:43 +0530

    done

--- frontend/src/pages/Doctors.jsx
+++ frontend/src/pages/Doctors.jsx
@@ -1,64 +1,62 @@
 import React, { useContext, useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { AppContext } from "../context/AppContext";
 
 function Doctors() {
   const { speciality } = useParams();
   const [filterDoc, setFilterDoc] = useState([]);
+  const navigate = useNavigate();
 
   const { doctors } = useContext(AppContext);
 
-  const applyFilter =()=>{
-    if(speciality){
-      setFilterDoc(doctors.filter(doc => doc.speciality === speciality))
-    }else{
-      setFilterDoc(doctors)
+  const applyFilter = () => {
+    if (speciality) {
+      setFilterDoc(doctors.filter((doc) => doc.speciality === speciality));
+    } else {
+      setFilterDoc(doctors);
     }
-  }
+  };
 
-  useEffect(()=>{
-    applyFilter()
-  },[doctors,speciality])
+  useEffect(() => {
+    applyFilter();
+  }, [doctors, speciality]);
 
   return (
     <div>
-      <p>Browse through the doctors specialist</p>
-      <div>
-        <div>
-          <p>General physician</p>
-          <p>Gynecologist</p>
-          <p>Dermatologist</p>
-          <p>Pediatricians</p>
-          <p>Neurologist</p>
-          <p>Gastroenterologist</p>
+      <p className="text-gray-600">Browse through the doctors specialist</p>
+      <div className="flex flex-col sm:flex-row items-start gap-5 mt-5">
+        <div className="flex flex-col gap-4 text-sm text-gray-600">
+          <p className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer`}>General physician</p>
+          <p className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer`}>Gynecologist</p>
+          <p className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer`}>Dermatologist</p>
+          <p className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer`}>Pediatricians</p>
+          <p className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer`}>Neurologist</p>
+          <p className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer`}>Gastroenterologist</p>
         </div>
-        <div>
-          <div className="w-full grid [grid-template-columns:repeat(auto-fill,minmax(200px,1fr))] gap-4 pt-5 gap-y-6 px-3 sm:px-0">
-            {filterDoc.slice(0, 10).map((item, index) => (
-              <div
-                className="border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500"
-                key={index}
-              >
-                <img
-                  className="bg-blue-50"
-                  src={item.image}
-                  alt={`image_${index}`}
-                />
-                <div className="p-4">
-                  <div className="flex items-center gap-2 text-sm text-green-500">
-                    <span className="w-2 h-2 bg-green-500 rounded-full animate-pulse"></span>
+        <div className="w-full grid [grid-template-columns:repeat(auto-fill,minmax(200px,1fr))] gap-4 pt-5 gap-y-6 px-3 sm:px-0">
+          {filterDoc.map((item, index) => (
+            <div
+              onClick={() => navigate(`/appointment/${item._id}`)}
+              className="border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500"
+              key={index}
+            >
+              <img
+                className="bg-blue-50"
+                src={item.image}
+                alt={`image_${index}`}
+              />
+              <div className="p-4">
+                <div className="flex items-center gap-2 text-sm text-green-500">
+                  <span className="w-2 h-2 bg-green-500 rounded-full animate-pulse"></span>
 
-                    <p>Available</p>
-                  </div>
-
-                  <p className="text-gray-900 text-lg font-medium">
-                    {item.name}
-                  </p>
-                  <p className="text-gray-600 text-sm">{item.speciality}</p>
+                  <p>Available</p>
                 </div>
+
+                <p className="text-gray-900 text-lg font-medium">{item.name}</p>
+                <p className="text-gray-600 text-sm">{item.speciality}</p>
               </div>
-            ))}
-          </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
