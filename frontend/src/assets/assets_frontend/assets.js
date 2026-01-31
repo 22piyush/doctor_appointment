cf73f636e9fba4044a554edf882bf668ccada51d
22piyush <piyushaglawe05@gmail.com>
2026-01-31 21:06:18 +0530

    done

--- frontend/src/assets/assets_frontend/assets.js
+++ frontend/src/assets/assets_frontend/assets.js
@@ -296,4 +296,18 @@ export const doctors = [
             line2: 'Circle, Ring Road, London'
         }
     },
+    {
+        _id: 'doc16',
+        name: 'Dr. Liana David',
+        image: doc15,
+        speciality: 'Gastroenterologist',
+        degree: 'MBBS',
+        experience: '4 Years',
+        about: 'Dr. David has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies. Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies.',
+        fees: 30,
+        address: {
+            line1: '37th Cross, Amreica',
+            line2: 'Circle, Ring Road, Sans Fransis'
+        }
+    },
 ]
