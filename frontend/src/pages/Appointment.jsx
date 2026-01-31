6f42e1c731239224a4e89604256776f118dc6aa0
22piyush <piyushaglawe05@gmail.com>
2026-01-31 22:27:01 +0530

    done

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -5,7 +5,7 @@ import { assets } from "../assets/assets_frontend/assets";
 
 function Appointment() {
   const { docId } = useParams();
-  const { doctors } = useContext(AppContext);
+  const { doctors , currencySymbol } = useContext(AppContext);
 
   const [docInfo, setDocInfo] = useState(null);
 
@@ -22,18 +22,44 @@ function Appointment() {
     docInfo && (
       <div>
         {/* Doctors Details  */}
-        <div>
+        <div className="flex flex-col sm:flex-row gap-4">
           <div>
-            <img src={docInfo.image} alt="image" />
+            <img
+              className="bg-[#5F6FFF] w-full sm:max-w-72 rounded-lg"
+              src={docInfo.image}
+              alt="image"
+            />
           </div>
 
-          <div>
+          <div className="flex-1 border border-gary-400 rounded-lg p-8 py-7 bg-white mx-2 sm:mx-0 mt-[-80px] sm:mt-0">
             {/* Doc Info */}
-            <p>{docInfo.name} <img src={assets.verified_icon} alt="verified_icon" /></p>
-            <div>
-              <p>{docInfo.degree} - {docInfo.speciality}</p>
-              <button>{docInfo.experience}</button>
+            <p className="flex items-center gap-2 text-2xl font-medium text-gray-900">
+              {docInfo.name}{" "}
+              <img
+                className="w-5"
+                src={assets.verified_icon}
+                alt="verified_icon"
+              />
+            </p>
+            <div className="flex items-center gap-2 text-sm mt-1 text-gray-600">
+              <p>
+                {docInfo.degree} - {docInfo.speciality}
+              </p>
+              <button className="py-0.5 px-2 border text-xs rounded-full">
+                {docInfo.experience}
+              </button>
             </div>
+
+            {/* ------------------- Doctor About ------------------ */}
+            <div>
+              <p className="flex items-center gap-1 text-sm font-medium text-gray-900 mt-3">
+                About <img src={assets.info_icon} alt="info_icon" />
+              </p>
+              <p className="text-sm text-gray-500 max-w-[700px] mt-1">{docInfo.about}</p>
+            </div>
+
+            <p>Appointment Fee: <span>{currencySymbol}{docInfo.fees}</span></p>
+
           </div>
         </div>
       </div>
