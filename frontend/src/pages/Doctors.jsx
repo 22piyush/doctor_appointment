795b66fbd3581bbb142e352d83de78e3130ff439
22piyush <piyushaglawe05@gmail.com>
2026-02-01 23:57:48 +0530

    done

--- frontend/src/pages/Doctors.jsx
+++ frontend/src/pages/Doctors.jsx
@@ -27,7 +27,7 @@ function Doctors() {
       <p className="text-gray-600">Browse through the doctors specialist</p>
       <div className="flex flex-col sm:flex-row items-start gap-5 mt-5">
         <button className={`py-1 px-3 border rounded text-sm transition-all sm:hidden ${showFilter ? 'bg-[#5F6FFF] text-white' : ''}`} onClick={()=>setShowFilter(prev => !prev)}>Filters</button>
-        <div className="flex flex-col gap-4 text-sm text-gray-600">
+        <div className={`flex flex-col gap-4 text-sm text-gray-600 ${showFilter ? 'flex' : 'hidden sm:flex'}`}>
           <p
             onClick={() =>
               speciality === "General physician"
