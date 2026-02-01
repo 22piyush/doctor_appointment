c84961db636e022c21797f0eb6fac08dbab444f4
22piyush <piyushaglawe05@gmail.com>
2026-02-01 20:22:04 +0530

    done

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -12,14 +12,72 @@ function Login() {
 
   return (
     <form action="" className="min-h-[80vh] flex items-center">
-      <div>
-        <p>{state === 'Sign Up' ? "Create Account" : "Login"}</p>
-        <p>Please {state === 'Sign Up' ? "sign up" : "log in"} to book appointmnt</p>
+      <div className="flex flex-col gap-3 m-auto items-center p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-zinc-600 text-sm shadow-lg">
+        <p className="text-2xl font-semibold">
+          {state === "Sign Up" ? "Create Account" : "Login"}
+        </p>
+        <p>
+          Please {state === "Sign Up" ? "sign up" : "log in"} to book appointmnt
+        </p>
 
-        <div>
-          <p>Full Name</p>
-          <input type="text" onChange={(e)=> setName(e.target.value)} value={name}/>
+        {state === "Sign Up" && (
+          <div className="w-full">
+            <p>Full Name</p>
+            <input
+              className="border border-zinc-300 rounded w-full p-2 mt-1"
+              type="text"
+              onChange={(e) => setName(e.target.value)}
+              value={name}
+              required
+            />
+          </div>
+        )}
+        <div className="w-full">
+          <p>Email</p>
+          <input
+            className="border border-zinc-300 rounded w-full p-2 mt-1"
+            type="text"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            required
+          />
         </div>
+        <div className="w-full">
+          <p>Password</p>
+          <input
+            className="border border-zinc-300 rounded w-full p-2 mt-1"
+            type="text"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+            required
+          />
+        </div>
+
+        <button className="bg-[#5F6FFF] text-white w-full py-2 rounded-md text-base">
+          {state === "Sign Up" ? "Create Account" : "Login"}
+        </button>
+
+        {state === "Sign Up" ? (
+          <p>
+            Already have an account?{" "}
+            <span
+              onClick={() => setState("Login")}
+              className="text-[#5F6FFF] cursor-pointer underline"
+            >
+              Login here
+            </span>
+          </p>
+        ) : (
+          <p>
+            Create an new account?{" "}
+            <span
+              onClick={() => setState("Sign Up")}
+              className="text-[#5F6FFF] cursor-pointer underline"
+            >
+              click here
+            </span>
+          </p>
+        )}
       </div>
     </form>
   );
