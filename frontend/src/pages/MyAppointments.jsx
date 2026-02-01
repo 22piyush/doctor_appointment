9ba20689cdfae2d4e4816f41140d06fba651ce81
22piyush <piyushaglawe05@gmail.com>
2026-02-01 23:22:30 +0530

    done

--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -6,10 +6,15 @@ function MyAppointments() {
 
   return (
     <div>
-      <p>My Appointments</p>
+      <p className="pb-3 mt-12 font-medium text-zinc-700 border-b">
+        My Appointments
+      </p>
       <div>
         {doctors.slice(0, 2).map((item, index) => (
-          <div key={index}>
+          <div
+            className="grid grid-cols-[1fr_2fr] gap-4 sm:flex sm:gap-6 py-2 border-b"
+            key={index}
+          >
             <div>
               <img src={item.image} alt={`image_${index}`} />
             </div>
