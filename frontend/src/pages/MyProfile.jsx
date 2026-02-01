6961eeae215df9b705255373f384133779e10e6d
22piyush <piyushaglawe05@gmail.com>
2026-02-01 20:54:53 +0530

    done

--- frontend/src/pages/MyProfile.jsx
+++ frontend/src/pages/MyProfile.jsx
@@ -1,11 +1,31 @@
 import React, { useState } from 'react'
+import { assets } from "../assets/assets_frontend/assets";
 
 function MyProfile() {
 
-  const [userData, setUserData] = useState()
+  const [userData, setUserData] = useState({
+    name:"Edward Vincent",
+    image:assets.profile_pic,
+    email:'skldfsdklfnskdl@gmail.com',
+    phone:"+1 233 3213 43423",
+    address:{
+      line1:"234 sdfs sdfsd",
+      line2:"234 sdfsdfb sdf"
+    },
+    gender:"Male",
+    dob:'2202-323-3213'
+  });
+
+  const [isEdit, setIsEdit] = useState(false);
 
   return (
-    <div>MyProfile</div>
+    <div>
+      <img src={userData.image} alt="image" />
+
+      {/* {
+        isEdit ? 
+      } */}
+    </div>
   )
 }
 
