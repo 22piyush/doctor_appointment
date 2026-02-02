52796a19e1a28da329900a8d9eea3dae181beae0
22piyush <piyushaglawe05@gmail.com>
2026-02-02 22:12:17 +0530

    done

---
+++ backend/controllers/adminController.js
@@ -0,0 +1,23 @@
+
+
+
+
+// API For adding Doctor 
+
+const addDoctor = async (req, res) => {
+
+    try {
+
+        const { name, email, password, speciality, degree, experience, about, fees, address } = req.body;
+        const imageFile = req.file;
+
+        
+   
+
+    } catch (err) {
+
+    }
+
+}
+
+export { addDoctor }
