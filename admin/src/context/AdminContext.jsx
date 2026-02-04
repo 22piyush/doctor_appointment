9c7e411036241fa9929ea67f0536bc5af4a566c9
22piyush <piyushaglawe05@gmail.com>
2026-02-04 23:06:51 +0530

    done

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -1,16 +1,36 @@
 import { createContext, useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 export const AdminContext = createContext();
 
 const AdminContextProvider = (props) => {
   const [aToken, setAToken] = useState(localStorage.getItem("aToken") || "");
+  const [doctors, setDoctors] = useState([]);
 
   const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
+  const getAllDoctors = async () => {
+    try {
+      const { data } = await axios.get(`${backendUrl}/api/admin/all-doctors`, {
+        headers: { aToken },
+      });
+
+      if (data.success) {
+        toast.success(data.message);
+        setDoctors(data.doctors)
+      }
+    } catch (err) {
+      return toast.error(err);
+    }
+  };
 
   const value = {
     aToken,
     setAToken,
     backendUrl,
+    doctors,
+    getAllDoctors
   };
 
   return (
--- admin/src/pages/Admin/DoctorList.jsx
+++ admin/src/pages/Admin/DoctorList.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useContext, useEffect } from "react";
+import { AdminContext } from "../../context/AdminContext";
 
 function DoctorList() {
+  const { doctors, aToken, getAllDoctors } = useContext(AdminContext);
+  console.log(doctors);
+
+  useEffect(() => {
+    if (aToken) getAllDoctors();
+  }, [aToken]);
+
   return (
-    <div>DoctorList</div>
-  )
+    <div className="p-6 min-h-screen w-full">
+      <div className="bg-white max-w-4xl mx-auto p-6 rounded-lg shadow max-h-[80vh] show-scrollbar overflow-auto">
+        <div className="w-full grid [grid-template-columns:repeat(auto-fill,minmax(200px,1fr))] gap-4 pt-5 gap-y-6 px-3 sm:px-0">
+          {doctors.map((item, index) => (
+            <div
+              className="border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500"
+              key={index}
+            >
+              <img
+                className="bg-blue-50"
+                src={item.image}
+                alt={`image_${index}`}
+              />
+              <div className="p-4">
+                <div className="flex items-center gap-2 text-sm text-green-500">
+                  <span className="w-2 h-2 bg-green-500 rounded-full animate-pulse"></span>
+
+                  <p>{item.available ? "Available" : "Not Availabe"}</p>
+                </div>
+
+                <p className="text-gray-900 text-lg font-medium">{item.name}</p>
+                <p className="text-gray-600 text-sm">{item.speciality}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default DoctorList
+export default DoctorList;
