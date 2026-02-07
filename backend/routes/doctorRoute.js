a20c1b243ec59c3043b3181c60cd97f3f5545c7b
22piyush <piyushaglawe05@gmail.com>
2026-02-07 18:34:39 +0530

    done

--- backend/routes/doctorRoute.js
+++ backend/routes/doctorRoute.js
@@ -1,10 +1,12 @@
 import express from "express";
 
-import { allDoctorsList, loginDoctor } from "../controllers/doctorController.js";
+import { allDoctorsList, appointmentsDoctor, loginDoctor } from "../controllers/doctorController.js";
+import authDoctor from "../middlewares/authDoctor.js";
 
 
 const doctorRouter = express.Router()
 doctorRouter.get('/list', allDoctorsList);
 doctorRouter.post("/login", loginDoctor);
+doctorRouter.get("/appointments", authDoctor, appointmentsDoctor);
 
 export default doctorRouter;
